7fe5aacfbc125a5a12d63e655cf396b2c5e646fe
Tatiana Toledo <tatiana@Tatianas-MacBook-Pro.local>
2022-10-13 13:45:19 +0200

    iteration2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
+const canvas = document.querySelector("canvas");
+canvas.style.border = "2px solid black";
+const ctx = canvas.getContext("2d");
+const startScreen = document.querySelector(".game-intro");
+
+const background = new Image();
+background.src = "../images/road.png";
+const car = new Image();
+car.src = "../images/car.png";
+
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(car, 200, 500, 70, 140);
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    startScreen.style.display = "none";
+  }
 };
