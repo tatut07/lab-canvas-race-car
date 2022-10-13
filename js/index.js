1bb46e305a1d72fa1520b07ac69895b578a9590b
Tatiana Toledo <tatiana@Tatianas-MacBook-Pro.local>
2022-10-13 22:00:14 +0200

    iteration4

--- js/index.js
+++ js/index.js
@@ -16,10 +16,16 @@ const startBtn = document.querySelector(".start-button");
 const carStartingPoint = canvasWidth / 2 - 70;
 const carSpeed = 5;
 
+const obstacleStartPoint = canvasWidth / 2;
+
+let obstacleX = obstacleStartPoint;
 let carX = carStartingPoint;
 let carMovement = 0;
 let back1 = 0;
 let back2 = -canvas.height;
+let obstacleMovement = 0;
+
+// let frames = 0;
 
 let isGameOver = false;
 let gameId = 0;
@@ -27,6 +33,57 @@ let gameId = 0;
 const moveCar = () => {
   if (carX + carMovement <= canvasWidth - 70 && carX + carMovement >= 0) {
     carX += carMovement;
+  }
+};
+// class Component {
+//   constructor(width, height, color, x, y) {
+// this.width = width;
+// this.height = height;
+// this.color = color;
+// this.x = x;
+// this.y = y;
+// }
+
+// const myObstacles = [];
+
+// function obstacles() {
+//   for (i = 0; i < myObstacles.length; i++) {
+//     myObstacles[i].x += -1;
+//     myObstacles[i].update();
+//   }
+//   frames += 1;
+//   if (frames % 100 === 0) {
+//     let x = canvasHeight;
+//     let minHeight = 20;
+//     let maxHeight = 200;
+//     let height = Math.floor(
+//       Math.random() * (maxHeight - minHeight + 1) + minHeight
+//     );
+//     let minGap = 80;
+//     let maxGap = 200;
+//     let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+//     myObstacles.push(new Component(10, height, "green", x, 0));
+//     myObstacles.push(
+//       new Component(10, x - height - gap, "green", x, height + gap)
+//     );
+//   }
+// }
+
+const drawObstacle = () => {
+  ctx.beginPath();
+  ctx.fillStyle = "tomato";
+  ctx.fillRect(obstacleX, canvasHeight, 150, 20);
+  ctx.closePath();
+};
+const moveObstacle = () => {
+  if (
+    obstacleX + obstacleMovement <= canvasHeight &&
+    obstacleX + obstacleMovement >= 0
+  ) {
+    obstacleX += obstacleMovement;
+    obstacleMovement += 1;
+  } else {
+    obstacleX = 2;
   }
 };
 
@@ -51,6 +108,9 @@ const animate = () => {
   if (gameId === 2000) {
     isGameOver = true;
   }
+  drawObstacle();
+  moveObstacle();
+  // obstacles();
 };
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
@@ -75,12 +135,3 @@ function startGame() {
     carMovement = 0;
   });
 }
-
-//
-
-// window.addEventListener("load", () => {
-//   startBtn.addEventListener("click", () => {
-//     // start
-//     start();
-//   });
-// });
